7d8a83fb316af5d013bb2aca6882aa0d586e7d29
Lilly <lek7hj@virginia.edu>
2020-03-08 19:45:42 -0400

    deploy to test location

--- CapYelp/urls.py
+++ CapYelp/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from restaurant.views import redirect_view
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", redirect_view),
+    path("restaurant/", include("restaurant.urls")),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from .models import FoodPlace
 
 # Create your views here.
+def redirect_view(request):
+    response = redirect("restaurant/")
+    return response
+
+
+class IndexView(generic.ListView):
+    template_name = "restaurant/index.html"
+    model = FoodPlace
+
+
+class MapView(generic.ListView):
+    template_name = "restaurant/map.html"
+    model = FoodPlace
+
+
+class AboutView(generic.ListView):
+    template_name = "restaurant/map.html"
+    model = FoodPlace
+
+
+def get_current_location(request):
+    lat = POST.get("lat")
+    lon = POST.get("long")
+
